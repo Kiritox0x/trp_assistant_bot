f13d4bac974d573c7e043ee99b3e5adfecd82c01
Kirito <letrungkien179@gmail.com>
2017-08-30 14:03:54 +0700

    new mail api

--- template_editor/mails.py
+++ template_editor/mails.py
@@ -4,6 +4,7 @@ from template_editor.services import ContextRender
 from open_course.models import Classroom, Teacher, Assistant
 from template_editor.models import MailTemplate
 import datetime
+import logging
 
 class EmailSender:
 	@classmethod
@@ -25,12 +26,12 @@ class EmailSender:
 		try:
 			classroom = Classroom.objects.get(pk = class_id)
 		except Classroom.DoesNotExist:
-			raise Exception("Classroom not exist")
+			logging.exception("Classroom not exist")
 
 		try:
 			template = MailTemplate.objects.get(pk = template_id)
 		except MailTemplate.DoesNotExist:
-			raise Exception("Mail Template does not exist")
+			logging.exception("Mail Template does not exist")
 
 		content = model_to_dict(classroom)
 		targets = []
@@ -39,27 +40,26 @@ class EmailSender:
 			content['start_date'] = classroom.start_date.isoformat()
 		if isinstance(classroom.finish_date, datetime.date):
 			content['finish_date'] = classroom.finish_date.isoformat()
-		if isinstance(classroom.teacher, open_course.models.Teacher):
+		if isinstance(classroom.teacher, Teacher):
 			content['teacher'] = classroom.teacher.name
 			targets.append(classroom.teacher.topica_email)
 		else:
-			raise Exception("Teacher not exist")
-		if isinstance(classroom.assistant, open_course.models.Assistant):
+			logging.exception("Teacher not exist")
+		if isinstance(classroom.assistant, Assistant):
 			content['assistant'] = classroom.assistant.name
 
-		for key in content:
+		for key in content.keys():
 			if content[key] == None:
 				content.pop(key)
 		print('Sent email to ' + str(targets))
-		return send_message(template.title, template.context, targets, title_dict=content, content_dict=content)
+		return self.send_message(template.title, template.context, targets, title_dict=content, content_dict=content)
 
+class WelcomeLetterSender:
+	def get_instances(self):
+		try:
+			classrooms = Classroom.objects.all()
+		except Classroom.DoesNotExist as e:
+			logging.exception(e)
+		return classrooms
 
-# class ClrWelcomeReminder:
-# 	def get_instances(self):
-# 		try:
-# 			classrooms = Classroom.objects.all()
-# 		except Classroom.DoesNotExist as e:
-# 			raise Exception(e)
-# 		return classrooms
-
-# 	def 
+		
